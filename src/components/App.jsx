a2318c2fa3ae39b6aa57f5cb99e661826d5cb4cc
PavloLysianyi <145061284+PavloLysianyi@users.noreply.github.com>
2024-01-17 19:34:34 +0200

    corrected

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,105 +1,10 @@
 import React from 'react';
-import { Puff } from 'react-loader-spinner';
-
-class Searchbar extends React.Component {
-  state = {
-    query: '',
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.onSubmit(this.state.query);
-  };
-
-  render() {
-    return (
-      <header className="Searchbar">
-        <form className="SearchForm" onSubmit={this.handleSubmit}>
-          <button type="submit" className="SearchForm-button"></button>
-          <input
-            className="SearchForm-input"
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Шукати зображення та фотографії"
-            value={this.state.query}
-            onChange={e => this.setState({ query: e.target.value })}
-          />
-        </form>
-      </header>
-    );
-  }
-}
-
-const ImageGalleryItem = ({ image, onImageClick }) => (
-  <li
-    className="ImageGalleryItem"
-    onClick={() => onImageClick(image.largeImageURL)}
-  >
-    <img className="ImageGalleryItem-image" src={image.webformatURL} alt="" />
-  </li>
-);
-
-const ImageGallery = ({ images, onImageClick }) => (
-  <ul className="ImageGallery">
-    {images.map(image => (
-      <ImageGalleryItem
-        key={image.id}
-        image={image}
-        onImageClick={onImageClick}
-      />
-    ))}
-  </ul>
-);
-
-const Button = ({ onLoadMore, hasMoreImages }) => (
-  <button
-    type="button"
-    className="Button"
-    onClick={onLoadMore}
-    style={{ display: hasMoreImages ? 'block' : 'none' }}
-  >
-    Завантажити ще
-  </button>
-);
-
-class Modal extends React.Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-
-  handleOverlayClick = e => {
-    if (e.target === e.currentTarget) {
-      this.props.onClose();
-    }
-  };
-
-  render() {
-    return (
-      <div className="Overlay" onClick={this.handleOverlayClick}>
-        <div className="Modal">
-          <img src={this.props.image} alt="" />
-        </div>
-      </div>
-    );
-  }
-}
-
-const Loader = () => (
-  <div className="loader">
-    <Puff color="#00BFFF" height={100} width={100} />
-  </div>
-);
+import Searchbar from './Searchbar';
+import ImageGallery from './ImageGallery';
+import Button from './Button';
+import Modal from './Modal';
+import Loader from './Loader';
+import fetchImages from './api';
 
 class App extends React.Component {
   state = {
@@ -123,7 +28,8 @@ class App extends React.Component {
   };
 
   handleLoadMore = () => {
-    this.fetchImages(this.state.searchQuery, this.state.currentPage + 1);
+    const nextPage = this.state.currentPage + 1;
+    this.fetchImages(this.state.searchQuery, nextPage);
   };
 
   handleImageClick = largeImageURL => {
@@ -137,25 +43,14 @@ class App extends React.Component {
   fetchImages = (query, page) => {
     this.setState({ isLoading: true });
 
-    const apiKey = '41251616-25bd2bca1571a95c770fcbb5d';
-    const perPage = 12;
-
-    const apiUrl = `https://pixabay.com/api/?q=${query}&page=${page}&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=${perPage}`;
-
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        if (data.hits.length === 0) {
-          this.setState({ hasMoreImages: false });
-        } else {
-          this.setState(prevState => ({
-            images: [...prevState.images, ...data.hits],
-            currentPage: page,
-            hasMoreImages: page < Math.ceil(data.totalHits / perPage),
-          }));
-        }
+    fetchImages(query, page)
+      .then(({ images, hasMoreImages }) => {
+        this.setState(prevState => ({
+          images: [...prevState.images, ...images],
+          currentPage: page,
+          hasMoreImages,
+        }));
       })
-      .catch(error => console.error('Error fetching images:', error))
       .finally(() => this.setState({ isLoading: false }));
   };
 
