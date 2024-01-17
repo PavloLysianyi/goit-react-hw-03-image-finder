d62fdae3a8c032292e3432a6ef625a5b4faca16b
PavloLysianyi <145061284+PavloLysianyi@users.noreply.github.com>
2024-01-17 02:12:39 +0200

    corrected

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,138 +1,170 @@
 import React from 'react';
 import { Puff } from 'react-loader-spinner';
 
-const Searchbar = ({ onSubmit }) => {
-  const [query, setQuery] = React.useState('');
+class Searchbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+    };
+  }
 
-  const handleSubmit = e => {
+  handleSubmit = e => {
     e.preventDefault();
-    onSubmit(query);
+    this.props.onSubmit(this.state.query);
   };
 
-  return (
-    <header className="Searchbar">
-      <form className="SearchForm" onSubmit={handleSubmit}>
-        <button type="submit" className="SearchForm-button"></button>
-        <input
-          className="SearchForm-input"
-          type="text"
-          autoComplete="off"
-          autoFocus
-          placeholder="Search images and photos"
-          value={query}
-          onChange={e => setQuery(e.target.value)}
+  render() {
+    return (
+      <header className="Searchbar">
+        <form className="SearchForm" onSubmit={this.handleSubmit}>
+          <button type="submit" className="SearchForm-button"></button>
+          <input
+            className="SearchForm-input"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            value={this.state.query}
+            onChange={e => this.setState({ query: e.target.value })}
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+class ImageGalleryItem extends React.Component {
+  render() {
+    const { image, onImageClick } = this.props;
+    return (
+      <li
+        className="ImageGalleryItem"
+        onClick={() => onImageClick(image.largeImageURL)}
+      >
+        <img
+          className="ImageGalleryItem-image"
+          src={image.webformatURL}
+          alt=""
         />
-      </form>
-    </header>
-  );
-};
+      </li>
+    );
+  }
+}
 
-const ImageGalleryItem = ({ image, onImageClick }) => {
-  return (
-    <li
-      className="ImageGalleryItem"
-      onClick={() => onImageClick(image.largeImageURL)}
-    >
-      <img className="ImageGalleryItem-image" src={image.webformatURL} alt="" />
-    </li>
-  );
-};
+class ImageGallery extends React.Component {
+  render() {
+    const { images, onImageClick } = this.props;
+    return (
+      <ul className="ImageGallery">
+        {images.map(image => (
+          <ImageGalleryItem
+            key={image.id}
+            image={image}
+            onImageClick={onImageClick}
+          />
+        ))}
+      </ul>
+    );
+  }
+}
 
-const ImageGallery = ({ images, onImageClick }) => {
-  return (
-    <ul className="ImageGallery">
-      {images.map(image => (
-        <ImageGalleryItem
-          key={image.id}
-          image={image}
-          onImageClick={onImageClick}
-        />
-      ))}
-    </ul>
-  );
-};
+class Button extends React.Component {
+  render() {
+    const { onLoadMore, hasMoreImages } = this.props;
+    return (
+      <button
+        type="button"
+        className="Button"
+        onClick={onLoadMore}
+        style={{ display: hasMoreImages ? 'block' : 'none' }}
+      >
+        Load more
+      </button>
+    );
+  }
+}
 
-const Button = ({ onLoadMore, hasMoreImages }) => {
-  return (
-    <button
-      type="button"
-      className="Button"
-      onClick={onLoadMore}
-      style={{ display: hasMoreImages ? 'block' : 'none' }}
-    >
-      Load more
-    </button>
-  );
-};
-
-const Modal = ({ image, onClose }) => {
-  React.useEffect(() => {
-    const handleKeyDown = e => {
+class Modal extends React.Component {
+  componentDidMount() {
+    this.handleKeyDown = e => {
       if (e.code === 'Escape') {
-        onClose();
+        this.props.onClose();
       }
     };
 
-    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
 
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, [onClose]);
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
 
-  const handleOverlayClick = e => {
+  handleOverlayClick = e => {
     if (e.target === e.currentTarget) {
-      onClose();
+      this.props.onClose();
     }
   };
 
-  return (
-    <div className="Overlay" onClick={handleOverlayClick}>
-      <div className="Modal">
-        <img src={image} alt="" />
+  render() {
+    return (
+      <div className="Overlay" onClick={this.handleOverlayClick}>
+        <div className="Modal">
+          <img src={this.props.image} alt="" />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-const Loader = () => {
-  return (
-    <div className="loader">
-      <Puff color="#00BFFF" height={100} width={100} />
-    </div>
-  );
-};
+class Loader extends React.Component {
+  render() {
+    return (
+      <div className="loader">
+        <Puff color="#00BFFF" height={100} width={100} />
+      </div>
+    );
+  }
+}
 
-const App = () => {
-  const [images, setImages] = React.useState([]);
-  const [currentPage, setCurrentPage] = React.useState(1);
-  const [searchQuery, setSearchQuery] = React.useState('');
-  const [isLoading, setIsLoading] = React.useState(false);
-  const [selectedImage, setSelectedImage] = React.useState(null);
-  const [hasMoreImages, setHasMoreImages] = React.useState(true);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: [],
+      currentPage: 1,
+      searchQuery: '',
+      isLoading: false,
+      selectedImage: null,
+      hasMoreImages: true,
+    };
+  }
 
-  const handleSearchSubmit = query => {
-    setSearchQuery(query);
-    setImages([]);
-    setCurrentPage(1);
-    setHasMoreImages(true);
-    fetchImages(query, 1);
+  handleSearchSubmit = query => {
+    this.setState({
+      searchQuery: query,
+      images: [],
+      currentPage: 1,
+      hasMoreImages: true,
+    });
+
+    this.fetchImages(query, 1);
   };
 
-  const handleLoadMore = () => {
-    fetchImages(searchQuery, currentPage + 1);
+  handleLoadMore = () => {
+    this.fetchImages(this.state.searchQuery, this.state.currentPage + 1);
   };
 
-  const handleImageClick = largeImageURL => {
-    setSelectedImage(largeImageURL);
+  handleImageClick = largeImageURL => {
+    this.setState({ selectedImage: largeImageURL });
   };
 
-  const handleCloseModal = () => {
-    setSelectedImage(null);
+  handleCloseModal = () => {
+    this.setState({ selectedImage: null });
   };
 
-  const fetchImages = (query, page) => {
-    setIsLoading(true);
+  fetchImages = (query, page) => {
+    this.setState({ isLoading: true });
 
     const apiKey = '41251616-25bd2bca1571a95c770fcbb5d';
     const perPage = 12;
@@ -143,29 +175,38 @@ const App = () => {
       .then(response => response.json())
       .then(data => {
         if (data.hits.length === 0) {
-          setHasMoreImages(false);
+          this.setState({ hasMoreImages: false });
         } else {
-          setImages(prevImages => [...prevImages, ...data.hits]);
-          setCurrentPage(page);
+          this.setState(prevState => ({
+            images: [...prevState.images, ...data.hits],
+            currentPage: page,
+          }));
         }
       })
       .catch(error => console.error('Error fetching images:', error))
-      .finally(() => setIsLoading(false));
+      .finally(() => this.setState({ isLoading: false }));
   };
 
-  return (
-    <div className="App">
-      <Searchbar onSubmit={handleSearchSubmit} />
-      <ImageGallery images={images} onImageClick={handleImageClick} />
-      {isLoading && <Loader />}
-      {images.length > 0 && !isLoading && (
-        <Button onLoadMore={handleLoadMore} hasMoreImages={hasMoreImages} />
-      )}
-      {selectedImage && (
-        <Modal image={selectedImage} onClose={handleCloseModal} />
-      )}
-    </div>
-  );
-};
+  render() {
+    const { images, isLoading, hasMoreImages, selectedImage } = this.state;
+
+    return (
+      <div className="App">
+        <Searchbar onSubmit={this.handleSearchSubmit} />
+        <ImageGallery images={images} onImageClick={this.handleImageClick} />
+        {isLoading && <Loader />}
+        {images.length > 0 && !isLoading && (
+          <Button
+            onLoadMore={this.handleLoadMore}
+            hasMoreImages={hasMoreImages}
+          />
+        )}
+        {selectedImage && (
+          <Modal image={selectedImage} onClose={this.handleCloseModal} />
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
